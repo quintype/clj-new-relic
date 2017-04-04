4c9648b06e00a6386c96d4c4c8a6a94d6a595e05
Tejas Dinkar <tejas@gja.in>
2017-04-05 01:51:23 +0530

    version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-new-relic "1.0.0"
+(defproject clj-new-relic "1.0.1"
   :description "Tools for annotating clojure functions with new relic"
   :url "http://github.com/quintype/clj-new-relic"
   :license {:name "Eclipse Public License"
