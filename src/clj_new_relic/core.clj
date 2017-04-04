edb9c8bac36076a10fd22a743cdbdbbbbdb7999f
Tejas Dinkar <tejas@gja.in>
2017-04-04 08:47:18 +0530

    Changing defn to defn-traced to be compatible with clj-newrelic. Also Renaming to invoke

--- src/clj_new_relic/core.clj
+++ src/clj_new_relic/core.clj
@@ -1,25 +1,23 @@
-(ns clj-new-relic.core
-  (:require [clojure.core :as core])
-  (:refer-clojure :exclude [defn]))
+(ns clj-new-relic.core)
 
 (set! *warn-on-reflection* true)
 
 (definterface ITraced
-  (run0 [])
-  (run1 [arg1])
-  (run2 [arg1 arg2])
-  (run3 [arg1 arg2 arg3])
-  (run4 [arg1 arg2 arg3 arg4])
-  (run_more [arg1 arg2 arg3 arg4 more]))
+  (invoke [])
+  (invoke [arg1])
+  (invoke [arg1 arg2])
+  (invoke [arg1 arg2 arg3])
+  (invoke [arg1 arg2 arg3 arg4])
+  (invoke [arg1 arg2 arg3 arg4 more]))
 
 (defonce error-message-printed (atom false))
-(core/defn print-error-message []
+(defn print-error-message []
   (when-not @error-message-printed
     (reset! error-message-printed true)
     (binding [*out* *err*]
       (println "WARNING: [CLJ-NEW-RELIC] Cannot Find com.newrelic.api.agent.Trace. Please add newrelic to project.clj to get clojure traces."))))
 
-(core/defn class-exists? [class-sym]
+(defn class-exists? [class-sym]
   (try
     (resolve class-sym)
     true
@@ -30,33 +28,33 @@
   (if (class-exists? 'com.newrelic.api.agent.Trace)
     `(deftype ~name [f#]
        ITraced
-       (^{com.newrelic.api.agent.Trace ~args} run0 [_] (f#))
-       (^{com.newrelic.api.agent.Trace ~args} run1 [_ arg1#] (f# arg1#))
-       (^{com.newrelic.api.agent.Trace ~args} run2 [_ arg1# arg2#] (f# arg1# arg2#))
-       (^{com.newrelic.api.agent.Trace ~args} run3 [_ arg1# arg2# arg3#] (f# arg1# arg2# arg3#))
-       (^{com.newrelic.api.agent.Trace ~args} run4 [_ arg1# arg2# arg3# arg4#] (f# arg1# arg2# arg3# arg4#))
-       (^{com.newrelic.api.agent.Trace ~args} run_more [_ arg1# arg2# arg3# arg4# more#] (apply f# arg1# arg2# arg3# arg4# more#)))
+       (^{com.newrelic.api.agent.Trace ~args} invoke [_] (f#))
+       (^{com.newrelic.api.agent.Trace ~args} invoke [_ arg1#] (f# arg1#))
+       (^{com.newrelic.api.agent.Trace ~args} invoke [_ arg1# arg2#] (f# arg1# arg2#))
+       (^{com.newrelic.api.agent.Trace ~args} invoke [_ arg1# arg2# arg3#] (f# arg1# arg2# arg3#))
+       (^{com.newrelic.api.agent.Trace ~args} invoke [_ arg1# arg2# arg3# arg4#] (f# arg1# arg2# arg3# arg4#))
+       (^{com.newrelic.api.agent.Trace ~args} invoke [_ arg1# arg2# arg3# arg4# more#] (apply f# arg1# arg2# arg3# arg4# more#)))
     (do
       (print-error-message)
       `(deftype ~name [f#]
          ITraced
-         (run0 [_] (f#))
-         (run1 [_ arg1#] (f# arg1#))
-         (run2 [_ arg1# arg2#] (f# arg1# arg2#))
-         (run3 [_ arg1# arg2# arg3#] (f# arg1# arg2# arg3#))
-         (run4 [_ arg1# arg2# arg3# arg4#] (f# arg1# arg2# arg3# arg4#))
-         (run_more [_ arg1# arg2# arg3# arg4# more#] (apply f# arg1# arg2# arg3# arg4# more#))))))
+         (invoke [_] (f#))
+         (invoke [_ arg1#] (f# arg1#))
+         (invoke [_ arg1# arg2#] (f# arg1# arg2#))
+         (invoke [_ arg1# arg2# arg3#] (f# arg1# arg2# arg3#))
+         (invoke [_ arg1# arg2# arg3# arg4#] (f# arg1# arg2# arg3# arg4#))
+         (invoke [_ arg1# arg2# arg3# arg4# more#] (apply f# arg1# arg2# arg3# arg4# more#))))))
 
-(defmacro defn [sym & args]
+(defmacro defn-traced [sym & args]
   (let [clazz-name (-> sym munge (str "_traced") symbol)]
     `(do
        (deftraced ~clazz-name {})
        (let [inner-function# (fn ~sym ~@args)
              traced# (new ~clazz-name inner-function#)]
-         (core/defn ~sym
-           ([] (.run0 traced#))
-           ([arg1#] (.run1 traced# arg1#))
-           ([arg1# arg2#] (.run2 traced# arg1# arg2#))
-           ([arg1# arg2# arg3#] (.run3 traced# arg1# arg2# arg3#))
-           ([arg1# arg2# arg3# arg4#] (.run4 traced# arg1# arg2# arg3# arg4#))
-           ([arg1# arg2# arg3# arg4# & more#] (.run_more traced# arg1# arg2# arg3# arg4# more#)))))))
+         (defn ~sym
+           ([] (.invoke traced#))
+           ([arg1#] (.invoke traced# arg1#))
+           ([arg1# arg2#] (.invoke traced# arg1# arg2#))
+           ([arg1# arg2# arg3#] (.invoke traced# arg1# arg2# arg3#))
+           ([arg1# arg2# arg3# arg4#] (.invoke traced# arg1# arg2# arg3# arg4#))
+           ([arg1# arg2# arg3# arg4# & more#] (.invoke traced# arg1# arg2# arg3# arg4# more#)))))))
