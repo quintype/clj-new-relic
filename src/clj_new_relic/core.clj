a81c94f8d5f7b50e0be6cc485c044ae2b551b8f2
Miguel Ping <miguel.ping@indiecampers.com>
2019-02-21 10:35:19 +0000

    allow to docstring to be passed

--- src/clj_new_relic/core.clj
+++ src/clj_new_relic/core.clj
@@ -1,15 +1,19 @@
 (ns clj-new-relic.core
   (:require [clj-new-relic.impl :as impl]))
 
-(defmacro defn-traced [sym & args]
-  (let [clazz-name (-> sym munge (str "_traced") symbol)]
+(defmacro defn-traced [sym & args2]
+  (let [clazz-name (-> sym munge (str "_traced") symbol)
+        docstring? (string? (first args2))
+        docstring  (if docstring? (first args2) "")
+        args       (if docstring? (rest args2) args2)]
     `(do
        (impl/define-traced-class ~clazz-name ~(assoc (-> sym meta :newrelic) :metricName (str "Clojure/" *ns* \/ sym)))
        (let [inner-function# (fn ~sym ~@args)
-             traced# (new ~clazz-name inner-function#)]
+             traced#         (new ~clazz-name inner-function#)]
          (defn ~sym
+           ~docstring
            ([] (.invoke traced#))
-           ([arg1#] (.invoke traced# arg1#))
+           ([arg1#]  (.invoke traced# arg1#))
            ([arg1# arg2#] (.invoke traced# arg1# arg2#))
            ([arg1# arg2# arg3#] (.invoke traced# arg1# arg2# arg3#))
            ([arg1# arg2# arg3# arg4#] (.invoke traced# arg1# arg2# arg3# arg4#))
--- test/clj_new_relic/core_test.clj
+++ test/clj_new_relic/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [clj-new-relic.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn-traced increment [a] (+ a 1))
+(defn-traced increment-with-doc "increments" [a] (+ a 1))
+(defn-traced- increment-private-with-doc "increments" [a] (+ a 1))
+
+(deftest defn-traced-test
+  (testing "defn-traced can be invoked"
+    (is (= 2 (increment 1))))
+
+  (testing "defn-traced generates doc and works"
+    (is (= 2 (increment 1)))
+    (is (= "increments" (:doc (meta #'increment-with-doc)))))
+
+  (testing "defn-traced- generates doc and works"
+    (is (= 2 (increment 1)))
+    (is (= "increments" (:doc (meta #'increment-private-with-doc))))))
+
